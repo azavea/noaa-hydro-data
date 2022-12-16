aca480e84208280ed1c0e54f9ada845fa112a84f
jpolchlo <jpolchlopek@azavea.com>
2022-12-16 16:20:18 -0500

    [wip]

--- src/python-scripts/compute-base-flow.py
+++ src/python-scripts/compute-base-flow.py
@@ -1,13 +1,17 @@
+import logging
+
 import dask_gateway
 import numpy as np
 import pandas as pd
 import xarray as xr
 
 
-print("Sample text")
+logger = logging.getLogger("DaskWorkflow")
+
+logger.error("Test")
 gw = dask_gateway.Gateway(auth="jupyterhub")
-print(type(gw.auth))
-print(gw.auth.api_token)
+logger.warn(f"Using auth of type {type(gw.auth)}")
+logger.info(f"Auth token: {gw.auth.api_token}")
 
 opts = gw.cluster_options()
 opts.worker_memory = 10
@@ -20,7 +24,8 @@ else:
 client = cluster.get_client()
 
 nwm_uri = 's3://noaa-nwm-retrospective-2-1-zarr-pds/chrtout.zarr'
-recent = xr.open_zarr(nwm_uri).where(ds['time'] >= np.datetime64('2010-01-01'), drop=True)
+ds = xr.open_zarr(nwm_uri)
+recent = ds.where(ds['time'] >= np.datetime64('2010-01-01'), drop=True)
 weekly_avg = recent.streamflow.groupby('time.week').mean().rename('mean')
 weekly_std = recent.streamflow.groupby('time.week').std().rename('std')
 base_flow = xr.merge([weekly_avg, weekly_std])
